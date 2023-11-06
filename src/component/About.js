a7ebf806c78e4ccd75cb1a70b2a133d2e0cf303c
priya kumari <103124555+priya20003@users.noreply.github.com>
2023-11-06 22:40:36 +0530

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -10,10 +10,12 @@ const About = () => {
     
     < div className='about_card '>
     <h2>ABOUT OUR WEBSITE</h2>
+  <div className='about_sec'>
     Matrix is here to transform the way you travel on the metro. Say goodbye to the hassle of physical tickets and cards, and welcome a seamless, ticketless, and cardless journey. Our website is dedicated to providing a convenient and efficient metro experience by harnessing the power of advanced technologies such as location-based charging and contactless payments.
 
 We understand the frustrations of waiting in long queues and dealing with the inconvenience of traditional ticketing systems. With Marix, all you need is your smartphone or any contactless payment device. Simply register on our website, and you'll gain access to a world of effortless metro travel.
     </div>
+     </div>
     </div>
     </>
   );
